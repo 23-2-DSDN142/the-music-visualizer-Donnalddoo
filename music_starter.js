b0ad8cc8104a412c85ab04170f418f158de4df79
Blulowy <140657391+Blulowy@users.noreply.github.com>
2023-10-24 22:18:32 +1300

    New Idea
    Floating eye, start of clouds

--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,53 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
+  background(237,77,55)
   textFont('Helvetica'); // please use CSS safe fonts
-  rectMode(CENTER)
+  rectMode(CENTER);
+  ellipseMode(CENTER);
+  angleMode(DEGREES);
   textSize(24);
+  noStroke();
+  fill(255)
+  let eyex = 100
+  let eyey = 50
+  let irisx = eyex + 5
+  let irisy = eyey + 18
 
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
+  // EYE  
+  push();
+  fill(0);
+  scale(0.7);
+  translate(200,200);
+  rotate(-25);
 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+  beginShape(); //EYELIDS
+  vertex(eyex-250,eyey);
+  bezierVertex(eyex-150,eyey,eyex-100,eyey+45,eyex-80,eyey+65);
+  bezierVertex(eyex-40,eyey+105,eyex+40,eyey+105,eyex+80,eyey+65);
+  bezierVertex(eyex+100,eyey+45,eyex+150,eyey,eyex+250,eyey);
+  bezierVertex(eyex+150,eyey,eyex+100,eyey-45,eyex+80,eyey-65);
+  bezierVertex(eyex+40,eyey-105,eyex-40,eyey-105,eyex-80,eyey-65);
+  bezierVertex(eyex-100,eyey-45,eyex-150,eyey,eyex-250,eyey);
+  endShape(); 
+
+  fill(255);
+  ellipse(eyex, eyey+5, 190, 160); // SCLERA
+
+  fill(0);
+  circle(irisx,irisy,130); // IRIS
+
+  fill(237,77,55);
+  circle(irisx+4,irisy+13,40,); // PUPIL
+  pop();
+  // EYE;
+
+  // CLOUDS
+  let centerSize =
+  arc(600,290,90,90,135,45,OPEN);
+  fill(100)
+  arc(670,295,70,70,135,45,OPEN);
+  fill(0)
+  arc(550,300,50,50,135,45,OPEN);
+  // CLOUDS;
 }
